d328edd9ae7da37dd653bdd03b7d908a578b8abc
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-26 18:58:14 -0400

    Create GameJoltKeys.hx

---
+++ source/GameJoltKeys.hx
@@ -0,0 +1,7 @@
+package;
+
+class GameJoltKeys
+{
+    public static var id:Int = 708124; // Put your game's ID here
+    public static var key:String = "d7ba70f0ee3d59de679b28851cc5ac30"; // Put your game's private API key here
+}
