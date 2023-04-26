c04b7d64ac42c5de7ae6c789cf91bc15176408af
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-04-26 06:59:49 -0400

    Add files via upload

---
+++ assets/preload/characters/redshaggy.lua
@@ -0,0 +1,4 @@
+function onCreate()
+    setPropertyFromClass('GameOverSubstate', 'characterName', 'redshaggy-dead'); --Character json file for the death animation
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'death/fnf_loss_sfx-shaggy');
+end
